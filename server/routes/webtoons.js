1dacdf1bc48317da4b64b31e4f07aadf73704d9f
Shreya Shahi <105261451+shreyashahi30@users.noreply.github.com>
2024-10-12 19:15:49 +0530

    Update webtoons.js

--- server/routes/webtoons.js
+++ server/routes/webtoons.js
@@ -1,1 +1,13 @@
+// server/routes/webtoons.js
+const express = require('express');
+const router = express.Router();
+const webtoonController = require('../controllers/webtoonController');
+
+// Route to get all webtoons
+router.get('/', webtoonController.getAllWebtoons);
+
+// Route to create a new webtoon (optional)
+router.post('/', webtoonController.createWebtoon);
+
+module.exports = router;
 
