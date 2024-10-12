41b530696c315c38b64afc6016b72a8a4e79c5f7
Shreya Shahi <105261451+shreyashahi30@users.noreply.github.com>
2024-10-12 19:16:05 +0530

    Update webtoonController.js

--- server/controllers/webtoonController.js
+++ server/controllers/webtoonController.js
@@ -1,1 +1,26 @@
+// server/controllers/webtoonController.js
+const Webtoon = require('../models/webtoon');
+
+// Get all webtoons
+exports.getAllWebtoons = async (req, res) => {
+    try {
+        const webtoons = await Webtoon.find();
+        res.status(200).json(webtoons);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching webtoons', error });
+    }
+};
+
+// Create a new webtoon (optional)
+exports.createWebtoon = async (req, res) => {
+    const { title, image, description } = req.body;
+
+    const newWebtoon = new Webtoon({ title, image, description });
+    try {
+        await newWebtoon.save();
+        res.status(201).json(newWebtoon);
+    } catch (error) {
+        res.status(500).json({ message: 'Error creating webtoon', error });
+    }
+};
 
