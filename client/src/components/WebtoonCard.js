078c15c2ce30a2eaeb3356dc3ceaa0914769ef1b
Shreya Shahi <105261451+shreyashahi30@users.noreply.github.com>
2024-10-12 19:07:01 +0530

    Update WebtoonCard.js

--- client/src/components/WebtoonCard.js
+++ client/src/components/WebtoonCard.js
@@ -1,1 +1,15 @@
+// src/components/WebtoonCard.js
+import React from 'react';
+
+const WebtoonCard = ({ title, image, description }) => {
+    return (
+        <div className="webtoon-card">
+            <img src={image} alt={`${title} cover`} className="webtoon-image" />
+            <h3 className="webtoon-title">{title}</h3>
+            <p className="webtoon-description">{description}</p>
+        </div>
+    );
+};
+
+export default WebtoonCard;
 
