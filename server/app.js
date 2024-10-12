112674b6f6f9ae927687421042e51c70b5552b59
Shreya Shahi <105261451+shreyashahi30@users.noreply.github.com>
2024-10-12 19:16:47 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,1 +1,21 @@
+// server/app.js
+const express = require('express');
+const connectDB = require('./database/db');
+const webtoonRoutes = require('./routes/webtoons');
+require('dotenv').config();
+
+const app = express();
+
+// Connect to database
+connectDB();
+
+// Middleware
+app.use(express.json());
+app.use('/api/webtoons', webtoonRoutes);
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
