d5deddc507757eea12ef7b40a51ff88acb6b59d1
Shreya Shahi <105261451+shreyashahi30@users.noreply.github.com>
2024-10-12 19:09:34 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,15 @@
+// src/App.js
+import React from 'react';
+import Home from './pages/Home';
+import './styles/App.css'; // Import main styles
+
+const App = () => {
+    return (
+        <div className="app">
+            <Home />
+        </div>
+    );
+};
+
+export default App;
 
