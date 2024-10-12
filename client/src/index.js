b31c2cc6814158ee285e3a6d4ff6bf76cc5c94b9
Shreya Shahi <105261451+shreyashahi30@users.noreply.github.com>
2024-10-12 19:09:53 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,13 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './styles/App.css'; // Import main styles
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
