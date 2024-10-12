feabcda2888759e3d3671c5c3095eca13a080f91
Shreya Shahi <105261451+shreyashahi30@users.noreply.github.com>
2024-10-12 19:07:33 +0530

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,1 +1,19 @@
+// src/components/Header.js
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="app-header">
+            <h1>Webtoon Wonderland</h1>
+            <nav>
+                <ul>
+                    <li><a href="#top-webtoons">Top Webtoons</a></li>
+                    <li><a href="#voting">Vote</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
 
