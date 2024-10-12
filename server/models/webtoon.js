8eb6f9caf717d114acb9b6c815fd44e6df638645
Shreya Shahi <105261451+shreyashahi30@users.noreply.github.com>
2024-10-12 19:15:31 +0530

    Update webtoon.js

--- server/models/webtoon.js
+++ server/models/webtoon.js
@@ -1,1 +1,12 @@
+// server/models/webtoon.js
+const mongoose = require('mongoose');
+
+const webtoonSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    image: { type: String, required: true },
+    description: { type: String, required: true },
+});
+
+const Webtoon = mongoose.model('Webtoon', webtoonSchema);
+module.exports = Webtoon;
 
